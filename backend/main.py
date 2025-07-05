1458c831a5e782deb0307f8c04ede6eecfa5acb6
Parth Sinha <sinhaparth555@gmail.com>
2025-07-05 12:58:47 +0100

    updated files

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from backend!")
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
