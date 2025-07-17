1a89840f19d500e22812fa3b60c76cb88dab6514
Parth Sinha <sinhaparth555@gmail.com>
2025-07-17 22:19:33 +0100

    added my policies page

---
+++ frontend/src/components/dashboard/MyPolicies.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Card, Text } from "@chakra-ui/react";
+
+export const MyPolicies = () => {
+  return (
+    <Box>
+      <Heading size="lg" mb={6}>My Insurance Policies</Heading>
+      <Card.Root>
+        <Card.Body>
+          <Text>Policy management coming soon...</Text>
+        </Card.Body>
+      </Card.Root>
+    </Box>
+  );
+};
