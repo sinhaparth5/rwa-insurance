9f2be41e42dc8fd4aa75647174515d455e28da05
Parth Sinha <sinhaparth555@gmail.com>
2025-07-20 13:25:54 +0100

    updated claims folder

--- frontend/src/components/dashboard/Claims.tsx
+++ frontend/src/components/dashboard/Claims.tsx
@@ -1,14 +1,7 @@
-import { Box, Heading, Card, Text } from "@chakra-ui/react";
+import { ClaimsManagement } from "../ClaimsManagement";
 
 export const Claims = () => {
   return (
-    <Box>
-      <Heading size="lg" mb={6}>Insurance Claims</Heading>
-      <Card.Root>
-        <Card.Body>
-          <Text>Claims management coming soon...</Text>
-        </Card.Body>
-      </Card.Root>
-    </Box>
+    <ClaimsManagement />
   );
 };
